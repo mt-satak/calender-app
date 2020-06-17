86b144af6c1515a36235eadcda6236a4d92b0c59
mt-satak <sakiware@gmail.com>
2020-06-18 00:43:17 +0900

    study_done

--- redux-sample/src/components/Counter/container.jsx
+++ redux-sample/src/components/Counter/container.jsx
@@ -1,3 +1,22 @@
+import { connect } from "react-redux";
+import { increment, decrement } from "../../redux/count/actions";
 import Counter from "./presentation";
 
-export default Counter;
+// const mapStateProps = ({ count }) => ({ count }); // ←下記と同義の書き方
+const mapStateProps = state => {
+  return { count: state.count };
+};
+
+const mapDispatchProps = dispatch => ({
+  increment: count => {
+    dispatch(increment(count));
+  },
+  decrement: count => {
+    dispatch(decrement(count));
+  }
+});
+
+export default connect(
+  mapStateProps,
+  mapDispatchProps
+)(Counter);
--- redux-sample/src/index.js
+++ redux-sample/src/index.js
@@ -1,8 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
 import Counter from "./components/Counter/container";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import rootReducer from "./redux/rootReducer";
 
-const App = () => <Counter />;
+const store = createStore(rootReducer);
+
+const App = () => (
+  <Provider store={store}>
+    <Counter />
+  </Provider>
+);
 
 ReactDOM.render(<App />, document.getElementById("root"));
